2138b15a5bd878815195e34a19ceb5d3f1520195
WarninGod <gamerfreakin6@gmail.com>
2026-01-08 01:09:16 +0530

    Add JWT authentication to all report endpoints

--- api/reports.js
+++ api/reports.js
@@ -27,6 +27,11 @@ module.exports = async (req, res) => {
         return res.status(405).json({ error: 'Method not allowed' });
     }
 
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ error: 'No token provided' });
+    }
+
     try {
         const reportType = req.query.type || 'summary';
 
